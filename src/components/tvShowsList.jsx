21914d5e7836d682b52d6931750d2c6a912d7013
everinurmind <nurbol.sultanov@gmail.com>
2023-04-27 16:02:05 -0600

    Adjust filter/search future

--- src/components/tvShowsList.jsx
+++ src/components/tvShowsList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPopularTVShows } from '../redux/tvShowsSlice';
+import '../styles/TVShowsList.css';
 
 const TVShowList = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -30,13 +31,10 @@ const TVShowList = () => {
     <div>
       <input type="text" value={searchTerm} onChange={handleSearchTermChange} placeholder="Search TV Shows" />
       {searchTerm && (
-        <ul>
+        <ul className="tv-show-list">
           {filteredTVShows.map((tvShow) => (
             <li key={tvShow.id}>
-              <Link to={`/tvShow/${tvShow.id}`}>
-                <img src={`https://image.tmdb.org/t/p/w185${tvShow.poster_path}`} alt={`${tvShow.title} poster`} />
-                {tvShow.title}
-              </Link>
+              <Link to={`/tv-shows/${tvShow.id}`}>{tvShow.name}</Link>
             </li>
           ))}
         </ul>
--- src/pages/tvShowDetails.jsx
+++ src/pages/tvShowDetails.jsx
@@ -1,28 +1,57 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 import { fetchTVShowDetails } from '../redux/tvShowsSlice';
+import '../styles/Details.css';
 
 const TVShowDetails = () => {
   const { tvShowId } = useParams();
   const dispatch = useDispatch();
-  const tvShowDetails = useSelector((state) => state.tvShows.tvShowDetails);
+  const tvShow = useSelector((state) => state.tvShows.tvShowDetails);
 
   useEffect(() => {
     dispatch(fetchTVShowDetails(tvShowId));
   }, [dispatch, tvShowId]);
 
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
   return (
-    <div>
-      {tvShowDetails && (
-        <div>
-          <h2>{tvShowDetails.name}</h2>
-          <p>{tvShowDetails.overview}</p>
+    <div className="container">
+      <div className="title">
+        <h2>{tvShow.name}</h2>
+        <button type="button" onClick={handleReturn} aria-label="Go back"><FaArrowLeft /></button>
+      </div>
+      {tvShow.id && (
+      <div className="tvShow-info">
+        <img src={`https://image.tmdb.org/t/p/w185${tvShow.poster_path}`} alt={`${tvShow.name} poster`} />
+        <div className="tvShow-info-text">
           <p>
-            Rating:
-            {tvShowDetails.vote_average}
+            <strong>First air date:</strong>
+            {tvShow.first_air_date}
+          </p>
+          <p>
+            <strong>Overview:</strong>
+            {tvShow.overview}
+          </p>
+          <p>
+            <strong>Vote average:</strong>
+            {tvShow.vote_average}
+          </p>
+          <p>
+            <strong>Number of seasons:</strong>
+            {tvShow.number_of_seasons}
+          </p>
+          <p>
+            <strong>Number of episodes:</strong>
+            {tvShow.number_of_episodes}
           </p>
         </div>
+      </div>
       )}
     </div>
   );
