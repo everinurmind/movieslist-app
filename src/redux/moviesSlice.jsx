dc6a38b4e049aad5de0ae814d0ab750d742326cd
everinurmind <nurbol.sultanov@gmail.com>
2023-04-27 11:28:22 -0600

    Add reducers

--- src/redux/moviesSlice.jsx
+++ src/redux/moviesSlice.jsx
@@ -1,4 +1,5 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const apiKey = '43f6e36cafa8a53b2e6d7e6c4ae05d7d';
 const baseUrl = 'https://api.themoviedb.org/3';
@@ -56,3 +57,38 @@ export const fetchMovieDetails = createAsyncThunk(
     return data;
   },
 );
+
+const moviesSlice = createSlice({
+  name: 'movies',
+  initialState: {
+    topRatedMovies: [],
+    upcomingMovies: [],
+    nowPlayingMovies: [],
+    popularMovies: [],
+    movieDetails: {},
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTopRatedMovies.fulfilled, (state, action) => {
+        state.topRatedMovies = action.payload;
+      })
+      .addCase(fetchUpcomingMovies.fulfilled, (state, action) => {
+        state.upcomingMovies = action.payload;
+      })
+      .addCase(fetchNowPlayingMovies.fulfilled, (state, action) => {
+        state.nowPlayingMovies = action.payload;
+      })
+      .addCase(fetchPopularMovies.fulfilled, (state, action) => {
+        state.popularMovies = action.payload;
+      })
+      .addCase(fetchMovieDetails.fulfilled, (state, action) => {
+        state.movieDetails = action.payload;
+      })
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.allMovies = action.payload;
+      });
+  },
+});
+
+export default moviesSlice.reducer;
